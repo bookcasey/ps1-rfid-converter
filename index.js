8e5561e6d065a5503552617a90c20e9812b4b74e
Casey Olson <casey@bookcasey.com>
2017-06-26 12:05:12 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const convert = (num) => {
+	let binary = num.toString(2);
+
+  // Add leading 0s to make 24 bits
+	if (binary.length < 24) {
+		binary = new Array(24 - binary.length).fill(0).join('') + binary;
+	}
+  // Convert first 8 bits to decimal, convert rest to decimal, and then concatinate
+	return `${parseInt(binary.slice(0,8), 2)}${parseInt(binary.slice(8), 2)}`;
+};
+
+console.log(convert(123456));
